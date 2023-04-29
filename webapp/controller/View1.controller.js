369b42298c5b20a84162a432954657cf10f89373
Jose Duarte <duartematiasduarte@gmail.com>
2023-04-29 11:35:35 +0000

    el programa mustra las dos pantalla dinamicas

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -82,13 +82,13 @@ sap.ui.define([
              for (var i in orders){
               orderItems.push( new sap.m.ColumnListItem({
                  cells : [
-                   new sap.m.Label({text :orders[i].OrdersID}),
+                   new sap.m.Label({text :orders[i].OrderID}),
                    new sap.m.Label({text :orders[i].Freight}),
                    new sap.m.Label({text :orders[i].ShipAddress}),
                  ]
               }))
              }
-
+               /**  */
              var newTable = new sap.m.Table({
               width:"auto",
               columns :[
@@ -110,9 +110,59 @@ sap.ui.define([
                     
               ],
               items : orderItems
+              } ).addStyleClass("sap.UiSmallMargin")
+             ordersTable.addItem(newTable);
+
                
-             } ).addStyleClass("sap.UiSmallMargin")
-             ordersTable.addItem(newTable)
+              
+             var newTableJSON =new sap.m.Table();
+             newTableJSON .setWidth("auto");
+             newTableJSON .addStyleClass("sapUiSmallMargin"); 
+
+             var columnOrderID =new sap.m.Column();
+             var labelOrderID =new sap.m.Label(); 
+             labelOrderID.bindProperty("text","i18n>orderID");
+             columnOrderID.setHeader(labelOrderID);
+             newTableJSON.addColumn(columnOrderID)
+
+             var columnFreight =new sap.m.Column();
+             var labelFreight =new sap.m.Label(); 
+             labelFreight.bindProperty("text","i18n>freight");
+             columnFreight.setHeader(labelFreight);
+             newTableJSON.addColumn(columnFreight)
+
+             var columnShipAddress =new sap.m.Column();
+             var labelShipAddress =new sap.m.Label(); 
+             labelShipAddress.bindProperty("text","i18n>shipAddress");
+             columnShipAddress.setHeader(labelShipAddress);
+             newTableJSON.addColumn(columnShipAddress);
+
+             
+
+             var columnListItem = new sap.m.ColumnListItem();
+             
+             var cellOrderID = new sap.m.Label();
+             cellOrderID.bindProperty("text","jsonEmployees>OrderID");
+             columnListItem.addCell(cellOrderID)
+
+             var cellFreight = new sap.m.Label();
+             cellFreight.bindProperty("text","jsonEmployees>Freight");
+             columnListItem.addCell(cellFreight);
+
+             var cellShipAddress = new sap.m.Label();
+             cellShipAddress.bindProperty("text","jsonEmployees>ShipAddress");
+             columnListItem.addCell(cellShipAddress);
+
+             var oBindingInfo = {
+              model: "jsonEmployees",
+              path: "Orders",
+              template: columnListItem
+             };
+
+              newTableJSON.bindAggregation( "items", oBindingInfo);
+              newTableJSON.bindElement( "jsonEmployees>" + oContext.getPath());
+              ordersTable.addItem(newTableJSON);
+
 
             }
           
